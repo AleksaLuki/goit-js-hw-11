b8b0d818aaab3f4780ea125eb7e285b73870a296
AleksaLuki <dimirtiy9000@gmail.com>
2023-03-27 16:15:37 +0300

    current work

---
+++ src/js/fetchImages.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export async function fetchImages(value, page) {
+  const url = 'https://pixabay.com/api/';
+  const key = '34748858-d31d3a783fed40cd5b22b2555';
+  const filter = `?key=${key}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`;
+
+  return await axios.get(`${url}${filter}`).then(response => response.data);
+}
